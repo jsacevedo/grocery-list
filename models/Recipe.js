e8225c4fe1aa6c0993e7ae199247088d3a29500b
jsacevedo <jordan_acevedo@yahoo.com>
2021-08-08 15:00:38 -0700

    #5 - Created Recipe Schema in mongoose

---
+++ models/Recipe.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const RecipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name for the recipe.'],
+  },
+  creator_name: {
+    type: String,
+    required: [true, 'You must be logged in to save a recipe.'],
+  },
+  ingredients: {
+    type: Array,
+  },
+});
+
+export default mongoose.models.Recipe || mongoose.model('Recipe', RecipeSchema);
